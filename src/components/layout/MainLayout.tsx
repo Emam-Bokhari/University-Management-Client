ca647e885c8fd7b660ff9cf457fe784f4fdfb080
Moshfiqur Rahman <studentemam@gmail.com>
2025-01-06 21:01:08 +0600

    feat: implement basic layout with sidebar and responsive design using Ant Design

---
+++ src/components/layout/MainLayout.tsx
@@ -0,0 +1,79 @@
+import { Layout, Menu, MenuProps } from "antd";
+import {
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from "@ant-design/icons";
+import { Fragment } from "react/jsx-runtime";
+
+const { Header, Sider, Content, Footer } = Layout;
+
+const items: MenuProps["items"] = [
+  {
+    key: "1",
+    label: "Dashboard",
+  },
+  {
+    key: "2",
+    label: "Profile",
+  },
+  {
+    key: "3",
+    label: "User Management",
+  },
+];
+
+export default function MainLayout() {
+  return (
+    <Fragment>
+      <Layout style={{ height: "100vh" }}>
+        <Sider
+          breakpoint="lg"
+          collapsedWidth="0"
+          onBreakpoint={(broken) => {
+            console.log(broken);
+          }}
+          onCollapse={(collapsed, type) => {
+            console.log(collapsed, type);
+          }}
+        >
+          {/* logo */}
+          <div
+            style={{
+              color: "white",
+              height: "4rem",
+              border: "2px solid red",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <h1 style={{ fontWeight: "normal" }}>PH-University</h1>
+          </div>
+          <Menu
+            theme="dark"
+            mode="inline"
+            defaultSelectedKeys={["4"]}
+            items={items}
+          />
+        </Sider>
+        <Layout>
+          <Header style={{ padding: 0 }} />
+          <Content style={{ margin: "24px 16px 0" }}>
+            <div
+              style={{
+                padding: 24,
+                minHeight: 360,
+              }}
+            >
+              content
+            </div>
+          </Content>
+          <Footer style={{ textAlign: "center" }}>
+            Ant Design ©{new Date().getFullYear()} Created by Ant UED
+          </Footer>
+        </Layout>
+      </Layout>
+    </Fragment>
+  );
+}
