c9bd4724faa36d710bb7287954e988cbc579e09e
Moshfiqur Rahman <studentemam@gmail.com>
2025-01-10 18:54:13 +0600

    feat: add routes generator function for creating route configuration

---
+++ src/pages/faculty/FacultyDashboard.tsx
@@ -0,0 +1,9 @@
+import { Fragment } from "react/jsx-runtime";
+
+export default function FacultyDashboard() {
+  return (
+    <Fragment>
+      <p>Faculty Dashboard</p>
+    </Fragment>
+  );
+}
---
+++ src/pages/student/StudentDashboard.tsx
@@ -0,0 +1,9 @@
+import { Fragment } from "react/jsx-runtime";
+
+export default function StudentDashboard() {
+  return (
+    <Fragment>
+      <p>Student Dashboard</p>
+    </Fragment>
+  );
+}
