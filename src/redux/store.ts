fa6a162defcabbd4ea803e86f50b8ab9678e7480
Moshfiqur Rahman <studentemam@gmail.com>
2025-01-12 15:44:11 +0600

    feat: add persisted authentication reducer using edux-persist

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,14 +1,27 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./features/auth/authSlice"
 import { baseApi } from "./api/baseApi";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+
+const persistConfig = {
+    key: "auth",
+    storage
+}
+
+const persistedAuthReducer = persistReducer(persistConfig, authReducer)
+
+
 
 export const store = configureStore({
     reducer: {
         [baseApi.reducerPath]: baseApi.reducer,
-        auth: authReducer
+        auth: persistedAuthReducer
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(baseApi.middleware)
 })
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
+
+export const persistor = persistStore(store)
