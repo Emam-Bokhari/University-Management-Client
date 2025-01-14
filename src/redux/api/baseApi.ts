62865e70d87de6f5bf53686c41b89a1b48b8feea
Moshfiqur Rahman <studentemam@gmail.com>
2025-01-14 13:37:18 +0600

    feat(auth): add refresh token mechanism for handling 401 errors

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,5 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { RootState } from "../store";
+import { logout, setUser } from "../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
     baseUrl: "http://localhost:3000/api/v1",
@@ -14,9 +15,39 @@ const baseQuery = fetchBaseQuery({
     }
 })
 
+const baseQueryWithRefreshToken = async (args, api, extraOptions) => {
+    let result = await baseQuery(args, api, extraOptions);
+    console.log(result);
+    if (result.error?.status === 401) {
+        console.log("Send refresh token")
+        const res = await fetch("http://localhost:3000/api/v1/auth/refresh-token", {
+            method: "POST",
+            credentials: "include",
+        })
+        const data = res.json();
+
+        if (data?.data?.accessToken) {
+            const user = (api.getState() as RootState).auth.user;
+
+            api.dispatch(
+                setUser({
+                    user: user,
+                    token: data?.data?.accessToken,
+                })
+            )
+
+            result = await baseQuery(args, api, extraOptions);
+        } else {
+            api.dispatch(logout())
+        }
+    }
+    return result;
+
+}
+
 export const baseApi = createApi({
     reducerPath: "baseApi",
-    baseQuery: baseQuery,
+    baseQuery: baseQueryWithRefreshToken,
     endpoints: () => ({
 
     })
