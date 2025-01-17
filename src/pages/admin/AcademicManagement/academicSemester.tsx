01cc63f651cfd89f1b37e5ff80e1c85dc95d3b05
Moshfiqur Rahman <studentemam@gmail.com>
2025-01-17 13:00:27 +0600

    feat: implement and design table component for show academicSemester data

--- src/pages/admin/AcademicManagement/academicSemester.tsx
+++ src/pages/admin/AcademicManagement/academicSemester.tsx
@@ -1,12 +1,55 @@
 import { Fragment } from "react/jsx-runtime";
 import { useGetAllSemestersQuery } from "../../../redux/features/academicManagementApi/academicManagement.api";
+import { Table, TableColumnsType } from "antd";
+
+interface DataType {
+  key: React.Key;
+  name: string;
+  age: number;
+  address: string;
+}
 
 export default function AcademicSemester() {
-  const { data } = useGetAllSemestersQuery(undefined);
-  console.log(data);
+  const { data: semesterData, isLoading } = useGetAllSemestersQuery(undefined);
+  console.log(semesterData);
+
+  const tableData = semesterData?.data.map(
+    ({ _id, name, year, startMonth, endMonth }) => ({
+      _id,
+      name,
+      year,
+      startMonth,
+      endMonth,
+    })
+  );
+
+  const columns: TableColumnsType<DataType> = [
+    {
+      title: "Semester Name",
+      dataIndex: "name",
+    },
+    {
+      title: "Year",
+      dataIndex: "year",
+    },
+    {
+      title: "Start Month",
+      dataIndex: "startMonth",
+    },
+    {
+      title: "End Month",
+      dataIndex: "endMonth",
+    },
+  ];
+
   return (
     <Fragment>
-      <p>Academic Semester component</p>
+      <Table<DataType>
+        columns={columns}
+        dataSource={tableData}
+        loading={isLoading}
+        showSorterTooltip={{ target: "sorter-icon" }}
+      />
     </Fragment>
   );
 }
--- src/redux/features/academicManagementApi/academicManagement.api.ts
+++ src/redux/features/academicManagementApi/academicManagement.api.ts
@@ -6,7 +6,14 @@ const academicManagementApi = baseApi.injectEndpoints({
             query: () => ({
                 url: "/academic-semesters",
                 method: "GET",
-            })
+            }),
+            transformResponse: (response) => {
+                console.log('Transformed Response:', response);
+                return {
+                    data: response.data,
+                    meta: response.meta,
+                };
+            }
         }),
         addAcademicSemester: builder.mutation({
             query: (data) => ({
